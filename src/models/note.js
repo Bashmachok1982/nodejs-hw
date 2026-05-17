abada82e4923ee1b079eed0c69fd1d418095015b
Bashmachok1982 <Bashmachok1982@gmail.com>
2026-05-17 07:33:55 +0300

    fix note model

--- src/models/note.js
+++ src/models/note.js
@@ -19,7 +19,6 @@ const noteSchema = new Schema(
       default: 'Todo',
       enum: TAGS,
       required: false,
-      index: true,
     },
   },
   {
@@ -27,5 +26,7 @@ const noteSchema = new Schema(
     versionKey: false,
   },
 );
+
+noteSchema.index({ tag: 1 });
 
 export const Note = model('Note', noteSchema);
