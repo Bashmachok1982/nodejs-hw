1b17c8565aa56d57d031014dd75a6c8b49c8f095
Bashmachok1982 <Bashmachok1982@gmail.com>
2026-05-17 07:19:30 +0300

    error debug

--- src/models/note.js
+++ src/models/note.js
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
@@ -16,19 +17,9 @@ const noteSchema = new Schema(
     tag: {
       type: String,
       default: 'Todo',
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      ],
+      enum: TAGS,
       required: false,
+      index: true,
     },
   },
   {
